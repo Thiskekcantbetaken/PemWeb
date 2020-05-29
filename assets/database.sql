5c9d04276751797eb2de12b8313277096677ddb3
rully saputra <rully.saputra4@gmail.com>
2020-05-29 15:02:53 +0700

    updated database

--- assets/database.sql
+++ assets/database.sql
@@ -1,58 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 29, 2020 at 10:01 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
 DROP DATABASE IF EXISTS shop;
 CREATE DATABASE shop;
 USE shop;
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT UNIQUE,
-  first_name VARCHAR(25) NOT NULL,
-  last_name VARCHAR(25) NOT NULL,
-  username VARCHAR(100) NOT NULL UNIQUE,
-  email VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
-  country VARCHAR(255) NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  contact VARCHAR(255) NOT NULL,
-  address VARCHAR(255) NOT NULL,
-  birthdate DATE NOT NULL,
-  gender VARCHAR(2) NOT NULL,
-  image VARCHAR (255) NOT NULL,
-  privilege_level INT NOT NULL,
-  PRIMARY KEY (id)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE admin (
-  id_admin INT NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  password VARCHAR (255) NOT NULL,
-  PRIMARY KEY (id_admin)
-);
+--
+-- Table structure for table `barang`
+--
 
-CREATE TABLE barang (
-  id_barang INT NOT NULL , 
-  nama_barang VARCHAR(255) NOT NULL,
-  harga_barang INT NOT NULL,
-  stok INT NOT NULL,
-  jumlah_terjual INT , 
-  foto VARCHAR (255),
-  PRIMARY KEY (id_barang) 
-);
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `harga_barang` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `jumlah_terjual` int(11) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `deskripsi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE transaksi (
-  waktu DATE , 
-  id_transaksi INT NOT NULL,
-  id INT NOT NULL , 
-  status INT NOT NULL,
-  PRIMARY KEY (id_transaksi),
-  FOREIGN KEY (id) references users (id)
-);
+--
+-- Dumping data for table `barang`
+--
 
-CREATE TABLE transaksi_detail (
-  id_barang INT NOT NULL ,
-  id_transaksi INT NOT NULL,
-  jumlah INT NOT NULL,
-  FOREIGN KEY (id_barang) REFERENCES barang (id_barang),
-  FOREIGN KEY (id_transaksi) REFERENCES transaksi (id_transaksi) 
-);
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `harga_barang`, `stok`, `jumlah_terjual`, `foto`, `deskripsi`) VALUES
+(1, 'Baju 1', 100000, 10, 0, NULL, 'Barang baru ori'),
+(2, 'Baju 2', 200000, 10, 0, NULL, 'Barang baru ori');
 
-INSERT INTO users VALUES
-(1, 'Test', 'Ting', 'coba', 'test@gmail.com', '$2y$10$y3e6GkIMkFXg3/Pk.hnzVuai9Supasyjv9lel/twyB9yRZagZUEnG','Indonesia','Jakarta','0812122313','JakartaBarat no 31', '2000-01-01', 'M','..\uploads\avenger.png', 2);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id_user` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `size` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `waktu` date DEFAULT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`waktu`, `id_transaksi`, `id_user`, `status`) VALUES
+('2020-05-29', 0, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi_detail`
+--
+
+CREATE TABLE `transaksi_detail` (
+  `id_barang` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `size` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi_detail`
+--
+
+INSERT INTO `transaksi_detail` (`id_barang`, `id_transaksi`, `jumlah`, `size`) VALUES
+(1, 0, 1, 'M'),
+(1, 0, 1, 'M'),
+(1, 0, 1, 'M'),
+(2, 0, 1, 'L');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `gender` varchar(2) NOT NULL,
+  `privilege_level` int(11) NOT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `birthdate`, `gender`, `privilege_level`, `image`) VALUES
+(1, 'Rully', 'Saputra', 'saputra523', 'rully.saputra4@gmail.com', '$2y$10$y3e6GkIMkFXg3/Pk.hnzVuai9Supasyjv9lel/twyB9yRZagZUEnG', '2020-05-29', 'M', 2, 'wrong-cross-png-4.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD KEY `id_user_fk` (`id_user`),
+  ADD KEY `id_barang_fk` (`id_barang`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id` (`id_user`);
+
+--
+-- Indexes for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD KEY `id_barang` (`id_barang`),
+  ADD KEY `transaksi_detail_itfk_1` (`id_transaksi`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `id_barang_fk` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `id_user_fk` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD CONSTRAINT `transaksi_detail_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `transaksi_detail_itfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
